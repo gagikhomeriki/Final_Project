8685ca4ea99e14476e8bb0e82849bff19bd77319
Gagi <110786058+gagikhomeriki@users.noreply.github.com>
2022-11-11 17:41:22 +0400

    finish

---
+++ script.js
@@ -0,0 +1,6 @@
+let burger = document.querySelector(".burger_menu")
+let menu = document.querySelector(".ul_ul")
+
+burger.addEventListener("click",function(){
+    menu.classList.toggle("active");
+})
